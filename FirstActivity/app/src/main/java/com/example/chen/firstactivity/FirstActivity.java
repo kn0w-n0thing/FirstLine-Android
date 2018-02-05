b56364f80291e787ba317580797b05b6b069023a
chenlili <chenlili@pset.suntec.net>
2018-02-05 20:02:39 +0800

    First commit
    Change-Id: I1967b035b29916d2eb8b012aea8abd76973793e9

---
+++ FirstActivity/app/src/main/java/com/example/chen/firstactivity/FirstActivity.java
@@ -0,0 +1,28 @@
+package com.example.chen.firstactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by chen on 18-2-5.
+ */
+
+public class FirstActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
