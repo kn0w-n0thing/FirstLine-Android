d74442fdf1575b6494c3e1777da7e9c44293ee1b
chenlili <chenlili@pset.suntec.net>
2018-03-24 22:07:18 +0800

    Add ActivityLifeCycleTest
    Change-Id: Iea687f0ed6632e1c7d631176a56e8b3cc8fd7de3

--- FirstActivity/app/src/main/java/com/example/chen/firstactivity/FirstActivity.java
+++ FirstActivity/app/src/main/java/com/example/chen/firstactivity/FirstActivity.java
@@ -1,7 +1,11 @@
 package com.example.chen.firstactivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,18 +15,44 @@ import android.widget.Toast;
  */
 
 public class FirstActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
-
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1",
-                        Toast.LENGTH_LONG).show();
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("http://www.baidu.com"));
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:112"));
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
