49747f8d41c977a66e9659d67981b9219fd785a9
llchen <llchen@telenav.cn>
2018-04-27 13:43:51 +0800

    Realize method reference

--- DataBindingTest/app/src/main/java/com/example/llchen/databindingtest/Handler.java
+++ DataBindingTest/app/src/main/java/com/example/llchen/databindingtest/Handler.java
@@ -1,14 +1,17 @@
 package com.example.llchen.databindingtest;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by chen on 18-4-26.
  */
 
 public class Handler {
-    public void onHandling(TextView view) {
-        view.setText("Hello world");
+    public void onHandling() {
+        Log.d(MainActivity.TAG, "onHandling");
     }
 }
--- DataBindingTest/app/src/main/java/com/example/llchen/databindingtest/MainActivity.java
+++ DataBindingTest/app/src/main/java/com/example/llchen/databindingtest/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.llchen.databindingtest;
 import android.support.v7.app.AppCompatActivity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.llchen.databindingtest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String TAG = "DataBindingTest";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         User user = new User("Test", "User");
         binding.setUser(user);
+        Handler handler = new Handler();
+        binding.setHandler(handler);
+        Log.d(MainActivity.TAG, "onCreate");
     }
 }
